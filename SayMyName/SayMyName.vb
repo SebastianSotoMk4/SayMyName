7bdb248c0772c51f686e7b5a7e76b2650d6c9db0
Sebastian Soto <thefirstcoolone@gmail.com>
2021-09-03 19:05:22 -0600

    URL and Modual name change

---
+++ SayMyName/SayMyName.vb
@@ -0,0 +1,20 @@
+﻿'Sebastian Soto
+'RCET0265
+'Fall 2021
+'Say My Name
+'https://github.com/SebastianSotoMk4/SayMyName.git
+Option Strict On
+Option Explicit On
+Module SayMyName
+    Sub Main()
+        'Create variable
+        Dim userName As String
+        'Ask a question with console
+        Console.WriteLine("Oh hello , I did'nt see you there. What is your name?")
+        'set variable to the users input
+        userName = Console.ReadLine()
+        'Reply to user with phrase and name
+        Console.WriteLine("Hello, " & userName & " Have a good one")
+        Console.ReadLine()
+    End Sub
+End Module
