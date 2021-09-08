df64b9c7bba361cdf494a90db78ee0d19f1ae018
Sebastian Soto <thefirstcoolone@gmail.com>
2021-09-07 20:01:47 -0600

    spelling

--- SayMyName/SayMyName.vb
+++ SayMyName/SayMyName.vb
@@ -13,18 +13,18 @@ Module SayMyName
         Console.WriteLine("Oh hello , I did'nt see you there. What is your name?") 'Ask a question with console
         userName = Console.ReadLine() 'set variable to the users input
 
-        If userName = "Sebastian" Or userName = "sebastian" Then 'If the name is reconized 
+        If userName = "Sebastian" Or userName = "sebastian" Then 'If the name is recognized 
             Console.WriteLine("Hey there, freshman!")            'then a message will be displayed
             variable = True
         ElseIf userName = "Emily" Or userName = "emily" Then     'Also names can be entered uppper or lowercase.
             Console.WriteLine("Put that cookie down!")
             variable = True
         ElseIf userName = "Joe" Or userName = "joe" Then
-            Console.WriteLine("This call may be recorded for training purposes.")
+            Console.WriteLine("...This call may be recorded for training purposes.")
             variable = True
         End If
-        If variable = False Then                                'If an unreconized name is entered
-            Console.WriteLine("Name not reconized")             'a message will be displayed.
+        If variable = False Then                                'If an unrecognized name is entered
+            Console.WriteLine("Name not reconized.")            'a message will be displayed.
         End If
         'ReadLine to view reply
         Console.ReadLine()
